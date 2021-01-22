c65b9700ac685e59aa3d7c2f4334343d9034aaab
repl.it user <replituser@example.com>
2021-01-22 03:19:25 +0000

    american british translator

--- components/translator.js
+++ components/translator.js
@@ -5,6 +5,92 @@ const britishOnly = require('./british-only.js')
 
 class Translator {
 
+  translateAndHighlight(strText, strLocale) {
+    let strTranslated = '' + strText;
+    if(strLocale == 'american-to-british') {
+      let keys = Object.keys(americanToBritishSpelling);
+      for(let key of keys) {
+        let regex = new RegExp(key+'(?![\\w-])', 'gi');
+        strTranslated = strTranslated.replace(regex, '<span class="highlight">'+americanToBritishSpelling[key]+'</span>');
+      }
+      keys = Object.keys(americanToBritishTitles);
+      for(let key of keys) {
+        let regex = new RegExp('(?<![\\w-])'+key+'(?![\\w-])', 'gi');
+        strTranslated = strTranslated.replace(regex, '<span class="highlight">'+americanToBritishTitles[key]+'</span>');
+      }
+      keys = Object.keys(americanOnly);
+      for(let key of keys) {
+        let regex = new RegExp('(?<![\\w-])'+key+'(?![\\w-])', 'gi');
+        strTranslated = strTranslated.replace(regex, '<span class="highlight">'+americanOnly[key]+'</span>');
+      }
+      let regex = /([0-9]{1,2})([:])([0-9]{1,2})/g;
+      strTranslated = strTranslated.replace(regex, '<span class="highlight">$1.$3</span>');
+    }
+    if(strLocale == 'british-to-american') {
+      let keys = Object.keys(americanToBritishSpelling);
+      for(let key of keys) {
+        let regex = new RegExp('(?<![\\w-])'+americanToBritishSpelling[key]+'(?![\\w-])', 'gi');
+        strTranslated = strTranslated.replace(regex, '<span class="highlight">'+key+'</span>');
+      }
+      keys = Object.keys(americanToBritishTitles);
+      for(let key of keys) {
+        let regex = new RegExp('(?<![\\w-])'+americanToBritishTitles[key]+'(?![\\w-])', 'gi');
+        strTranslated = strTranslated.replace(regex, '<span class="highlight">'+key+'</span>');
+      }
+      keys = Object.keys(britishOnly);
+      for(let key of keys) {
+        let regex = new RegExp('(?<![\\w-])'+key+'(?![\\w-])', 'gi');
+        strTranslated = strTranslated.replace(regex, '<span class="highlight">'+britishOnly[key]+'</span>');
+      }
+      let regex = /([0-9]{1,2})([\.])([0-9]{1,2})/g;
+      strTranslated = strTranslated.replace(regex, '<span class="highlight">$1:$3</span>');
+    }
+    return strTranslated;
+  }
+
+  translate(strText, strLocale) {
+    let strTranslated = '' + strText;
+    if(strLocale == 'american-to-british') {
+      let keys = Object.keys(americanToBritishSpelling);
+      for(let key of keys) {
+        let regex = new RegExp('(?<![\\w-])'+key+'(?![\\w-])', 'gi');
+        strTranslated = strTranslated.replace(regex, americanToBritishSpelling[key]);
+      }
+      keys = Object.keys(americanToBritishTitles);
+      for(let key of keys) {
+        let regex = new RegExp('(?<![\\w-])'+key+'(?![\\w-])', 'gi');
+        strTranslated = strTranslated.replace(regex, americanToBritishTitles[key]);
+      }
+      keys = Object.keys(americanOnly);
+      for(let key of keys) {
+        let regex = new RegExp('(?<![\\w-])'+key+'(?![\\w-])', 'gi');
+        strTranslated = strTranslated.replace(regex, americanOnly[key]);
+      }
+      let regex = /([0-9]{1,2})([:])([0-9]{1,2})/g;
+      strTranslated = strTranslated.replace(regex, '$1.$3');
+    }
+    if(strLocale == 'british-to-american') {
+      let keys = Object.keys(americanToBritishSpelling);
+      for(let key of keys) {
+        let regex = new RegExp('(?<![\\w-])'+americanToBritishSpelling[key]+'(?![\\w-])', 'gi');
+        strTranslated = strTranslated.replace(regex, key);
+      }
+      keys = Object.keys(americanToBritishTitles);
+      for(let key of keys) {
+        let regex = new RegExp('(?<![\\w-])'+americanToBritishTitles[key]+'(?![\\w-])', 'gi');
+        strTranslated = strTranslated.replace(regex, key);
+      }
+      keys = Object.keys(britishOnly);
+      for(let key of keys) {
+        let regex = new RegExp('(?<![\\w-])'+key+'(?![\\w-])', 'gi');
+        strTranslated = strTranslated.replace(regex, britishOnly[key]);
+      }
+      let regex = /([0-9]{1,2})([\.])([0-9]{1,2})/g;
+      strTranslated = strTranslated.replace(regex, '$1:$3');
+    }
+    return strTranslated;
+  }
+
 }
 
 module.exports = Translator;
--- routes/fcctesting.js
+++ routes/fcctesting.js
@@ -58,7 +58,7 @@ module.exports = function (app) {
       });
     });
     
-  let error;
+  var error;
   app.get('/_api/get-tests', cors(), function(req, res, next){
     console.log(error);
     if(!error && process.env.NODE_ENV === 'test') return next();
@@ -74,9 +74,9 @@ module.exports = function (app) {
     });
   });
   app.get('/_api/app-info', function(req, res) {
-    let hs = Object.keys(res._headers)
+    var hs = Object.keys(res._headers)
       .filter(h => !h.match(/^access-control-\w+/));
-    let hObj = {};
+    var hObj = {};
     hs.forEach(h => {hObj[h] = res._headers[h]});
     delete res._headers['strict-transport-security'];
     res.json({headers: hObj});
@@ -85,7 +85,7 @@ module.exports = function (app) {
 };
 
 function testFilter(tests, type, n) {
-  let out;
+  var out;
   switch (type) {
     case 'unit' :
       out = tests.filter(t => t.context.match('Unit Tests'));
